97e6cdc2aea58b9b447aa811be218a41a30c29a6
ehddlf5709 <ehddlf5709@naver.com>
2024-10-14 16:30:45 +0900

    Update provider_cus.tf

--- provider_cus.tf
+++ provider_cus.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0.0"  # 또는 최신 버전으로 업데이트
+      version = ">= 4.0.0"  # 또는 최신 버전으로 업데이트
     }
   }
 }
