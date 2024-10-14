85bccdeb41915971e173f66c5683ce3be023a3d9
ehddlf5709 <ehddlf5709@naver.com>
2024-10-14 17:03:25 +0900

    Update variable_cus.tf

--- variable_cus.tf
+++ variable_cus.tf
@@ -3,3 +3,8 @@ variable "image_id" {
   description = "The id of the machine image (AMI) to use for the server."
   default     = ""
 }
+variable "region" {
+  description = "The AWS region where resources will be created."
+  type        = string
+  default     = "ap-northeast-2"  # 원하는 리전으로 설정
+}
